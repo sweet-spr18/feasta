0207cfa3b49665e790a61076ddf302490b526d88
Neelesh Rastogi <neelr1997@users.noreply.github.com>
2018-04-29 03:32:59 -0400

    atom

--- phase3/android_Example/app/src/main/java/com/esoxjem/movieguide/listing/MoviesListingFragment.java
+++ phase3/android_Example/app/src/main/java/com/esoxjem/movieguide/listing/MoviesListingFragment.java
@@ -101,7 +101,7 @@ public class MoviesListingFragment extends Fragment implements MoviesListingView
 
         int columns;
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            columns = 2;
+            columns = 1;
         } else {
             columns = getResources().getInteger(R.integer.no_of_columns);
         }
