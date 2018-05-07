ad24847c74cac9e978a163275e317e1a37a4ccfb
Neelesh Rastogi <neelr1997@users.noreply.github.com>
2018-05-07 04:00:17 -0400

    Successfully attached the location screen with the Events

---
+++ phase3/android_from_scratch/app/src/main/java/sweet1/feasta/User.java
@@ -0,0 +1,23 @@
+package sweet1.feasta;
+
+public class User {
+
+    private String firstName;
+    private String userEmail;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
